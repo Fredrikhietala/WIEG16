f08333ad491b94cd16ce0b97b738e1c299895c71
Fredrikhietala <hietala.fredrik@gmail.com>
2017-03-30 20:22:26 +0200

    Solved 1.12-14 and almost 1.15

--- exercises_david/exercise_pancakerecipe.php
+++ exercises_david/exercise_pancakerecipe.php
@@ -9,4 +9,43 @@
 		public $salt;
 		public $butter;
 
+		public function canMakeSinglePancake() {
+			if ($this->flour >= 2.5/8 && $this->milk >= 6/8 && $this->egg >= 3/8 && $this->salt >= 0.5/8 && $this->butter >= 2/8) {
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+
+		public function makeSinglePancake() {
+			if ($this->canMakeSinglePancake()) {
+				echo "En pannkaka klar, mums <br>";
+				return true;
+			}		
+			else {
+				echo "Inte tillräckligt med ingredienser <br>";
+				return false;
+			}
+		}
+		public function makePancakes() {
+			for ($i=0; $i <= count($this->makeSinglePancake()) ; $i++) {
+				if ($this->makeSinglePancake()) {
+					$this->flour = $this->flour - 2.5/8;
+					$this->milk = $this->milk - 6/8;
+					$this->egg = $this->egg - 3/8;
+					$this->salt = $this->salt - 0.5/8;
+					$this->butter = $this->butter - 2/8;
+				}
+			}
+		}
 	}
+
+	$pancake1 = new PancakeRecipe();
+	$pancake1->flour 	= 5;
+	$pancake1->milk 	= 12;
+	$pancake1->egg 		= 6;
+	$pancake1->salt 	= 1;
+	$pancake1->butter 	= 4;
+
+	$pancake1->makePancakes();
